d789449822b782e2238f2343a9dcc1821422f04a
John Williams <jseyi09@gmail.com>
2024-04-15 19:42:22 -0400

    Addedmy code and labeled each part

--- tuff.py
+++ tuff.py
@@ -1,4 +1,5 @@
 
+'''Joaddan's Code'''
 class MenuItem:
     def __init__(self, name, keywords):
         self.name = name
@@ -23,4 +24,43 @@ class Menu:
             if keyword.lower() in food.keywords:
                 matches.append(food)
         return matches
->>>>>>> fdc6eb9b81c2c9646f7acf2e8ecb89e3779a89fd
+    
+'''John's Code'''
+def take_orders(customer_info, menu):
+    '''Takes orders from customers based on a given menu.
+    
+    Args:
+    customer_info (list of tuples): contains customer info, which consist of the
+                                    customer's name and their party size.
+    menu (Menu): Instance of the Menu class showing the available menu options
+    
+    Returns:
+    dict: A dictionary that has the order for each person with the keys being their names and values as the list of ordered food.
+    
+    
+    '''
+    orders = {}
+    for person, party_size in customer_info:
+        print(f'Ordering for person {person}:')
+        orders[person] = []
+        for i in range(party_size):
+            order = input('What would you like to eat today?')
+            matches = menu.find_food(order)
+            if matches:
+                print(f'Menu items matching "{order}":')
+                for match in matches:
+                    print(match.name)
+                waiter = input("Please choose an item you would like to eat:")
+                orders[person].append(waiter)
+            else:
+                print("Sorry, we don't have that item on the menu, please choose something else.")
+                orders[person].append(None)
+    return orders
+
+
+
+    
+
+
+
+
