187b6e6d9ffe17b03ce9be614ca6a36e697eecb8
Joaddan Cadet <joaddancadet44@gmail.com>
2024-05-06 21:47:23 -0400

    Added docstrings

--- final_draft.py
+++ final_draft.py
@@ -16,23 +16,49 @@ class MenuItem:
         self.price = price
         
     def __repr__(self):
+        
+        """
+        This returns a string representation of the MenuItem.
+        """
         return f"MenuItem('{self.name}', {self.price})"
 
 
 
 class Menu:
+    """
+    Represents a menu containing multiple MenuItem.
+    """
+   
     def __init__(self):
         self.foods = []
 
     def add_food(self, food):
+        """
+        Adds a MenuItem to the menu.
+
+        Args:
+            Food (MenuItem): The MenuItem is added to the menu.
+        """
         self.foods.append(food)
 
     def display_menu(self):
+        """
+        Displays the menu by printing each MenuItem's name and price.
+        """
         print("Menu:")
         for food in self.foods:
             print(f"{food.name}: ${food.price}")
 
     def find_food(self, keyword):
+        """
+        Finds MenuItem whose names contain the keyword.
+
+        Args:
+            keyword (str): This represents the keyword to search for in menu item.
+
+        Returns:
+            list: A list of MenuItem that matches the keyword.
+        """
         matches = []
         for food in self.foods:
             if keyword.lower() in food.name.lower():
@@ -40,6 +66,12 @@ class Menu:
         return matches
 
     def read_menu_from_csv(self, file_path):
+        """
+       This reads menu items from CSV file and adds them to the menu.
+
+        Args:
+            file_path (str): The path to the CSV file that contains the menu.
+        """
         with open(file_path, mode='r', newline='') as file:
             reader = csv.DictReader(file)
             for row in reader:
